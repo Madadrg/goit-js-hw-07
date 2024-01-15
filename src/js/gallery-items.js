9eab3b7bf4aa5d4998ee117c07a26b5612f607ef
Georgiana-Mădălina Dragomir <dragomirgeorgianamadalina@gmail.com>
2024-01-15 16:25:55 +0200

    Added all the pictures

--- src/js/gallery-items.js
+++ src/js/gallery-items.js
@@ -1,63 +1,47 @@
 export const galleryItems = [
   {
-    preview: "../src/alpine-mountains-original.jpg",
-    original: "../src/img",
+    preview: "../img/hokkaido-flower-preview.jpg",
+    original: "../img/hokkaido-flower-original.jpg",
     description: "Hokkaido Flower",
   },
   {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+    preview: "../img/container-haulage-preview.jpg",
+    original: "../img/container-haulage-original.jpg",
     description: "Container Haulage Freight",
   },
   {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+    preview: "../img/aerial-beach-view-preview.jpg",
+    original: "../img/aerial-beach-view-original.jpg",
     description: "Aerial Beach View",
   },
   {
-    preview:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+    preview: "../img/flower-blooms-preview.jpg",
+    original: "../img/flower-blooms-original.jpg",
     description: "Flower Blooms",
   },
   {
-    preview:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+    preview: "../img/alpine-mountains-preview.jpg",
+    original: "../img/alpine-mountains-original.jpg",
     description: "Alpine Mountains",
   },
   {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+    preview: "../img/mountain-lake-sailing-preview.jpg",
+    original: "../img/mountain-lake-sailing-original.jpg",
     description: "Mountain Lake Sailing",
   },
   {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    preview: "../img/alpine-spring-meadows-preview.jpg",
+    original: "../img/alpine-spring-meadows-original.jpg",
     description: "Alpine Spring Meadows",
   },
   {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    preview: "../img/nature-landscape-preview.jpg",
+    original: "../img/nature-landscape-original.jpg",
     description: "Nature Landscape",
   },
   {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    preview: "../img/lighthouse-coast-sea-preview.jpg",
+    original: "../img/lighthouse-coast-sea-original.jpg",
     description: "Lighthouse Coast Sea",
   },
 ];
