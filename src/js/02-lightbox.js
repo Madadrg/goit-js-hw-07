ea0068fba68b3c1e9988db6fc749bac9430e1491
Georgiana-Mădălina Dragomir <dragomirgeorgianamadalina@gmail.com>
2024-01-16 11:01:03 +0200

    Worked on second exercise

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+function renderGallery() {
+  const galleryContainer = document.querySelector(".gallery");
+
+  galleryItems.forEach((item) => {
+    const galleryItem = document.createElement("li");
+    galleryItem.classList.add("gallery__item");
+
+    const galleryLink = document.createElement("a");
+    galleryLink.classList.add("gallery__link");
+    galleryLink.href = item.original;
+
+    const galleryImage = document.createElement("img");
+    galleryImage.classList.add("gallery__image");
+    galleryImage.src = item.preview;
+    galleryImage.alt = item.description;
+
+    galleryLink.appendChild(galleryImage);
+    galleryItem.appendChild(galleryLink);
+    galleryContainer.appendChild(galleryItem);
+  });
+}
+
+function initializeSimpleLightbox() {
+  const gallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderGallery();
+  initializeSimpleLightbox();
+});
