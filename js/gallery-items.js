c146255dea925870ef7f164257a9cacb3e7d1ad3
Georgiana-Mădălina Dragomir <dragomirgeorgianamadalina@gmail.com>
2024-01-18 15:18:18 +0200

    Removed an error

---
+++ js/gallery-items.js
@@ -0,0 +1,47 @@
+export const galleryItems = [
+  {
+    preview: "../img/hokkaido-flower-preview.jpg",
+    original: "../img/hokkaido-flower-original.jpg",
+    description: "Hokkaido Flower",
+  },
+  {
+    preview: "../img/container-haulage-preview.jpg",
+    original: "..img/container-haulage-original.jpg",
+    description: "Container Haulage Freight",
+  },
+  {
+    preview: "../img/aerial-beach-view-preview.jpg",
+    original: "../img/aerial-beach-view-original.jpg",
+    description: "Aerial Beach View",
+  },
+  {
+    preview: "../img/flower-blooms-preview.jpg",
+    original: "../img/flower-blooms-original.jpg",
+    description: "Flower Blooms",
+  },
+  {
+    preview: "../img/alpine-mountains-preview.jpg",
+    original: "../img/alpine-mountains-original.jpg",
+    description: "Alpine Mountains",
+  },
+  {
+    preview: "../img/mountain-lake-sailing-preview.jpg",
+    original: "../img/mountain-lake-sailing-original.jpg",
+    description: "Mountain Lake Sailing",
+  },
+  {
+    preview: "../img/alpine-spring-meadows-preview.jpg",
+    original: "../img/alpine-spring-meadows-original.jpg",
+    description: "Alpine Spring Meadows",
+  },
+  {
+    preview: "../img/nature-landscape-preview.jpg",
+    original: "../img/nature-landscape-original.jpg",
+    description: "Nature Landscape",
+  },
+  {
+    preview: "../img/lighthouse-coast-sea-preview.jpg",
+    original: "../img/lighthouse-coast-sea-original.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+];
